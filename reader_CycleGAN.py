a9e2d121603aaa4f5ecee8031d25d9716d7339ab
bowen <zazakbw@gmail.com>
2018-04-19 22:18:02 +0200

    up4test

--- reader_CycleGAN.py
+++ reader_CycleGAN.py
@@ -54,6 +54,7 @@ class Reader():
     #image = utils.image_augmentation(image)
     image = tf.image.resize_images(image, size=self.image_size)
     image = utils.convert2float(image)
+    image = utils.image_augmentation(image)
     #print("self image size: {}".format(self.image_size))
     image.set_shape([self.image_size[0], self.image_size[1], 3])
     return image
