55e785a888b3fc915577f487aab6fce794eec5e0
bowen <zazakbw@gmail.com>
2018-04-28 20:57:15 +0200

    removed grayScale in reader, for real

--- reader_CycleGAN.py
+++ reader_CycleGAN.py
@@ -56,11 +56,11 @@ class Reader():
     image = utils.convert2float(image)
     # image = utils.image_augmentation(image)
     #3 channels
-    # image.set_shape([self.image_size[0], self.image_size[1], 3])
+    image.set_shape([self.image_size[0], self.image_size[1], 3])
 
     #1 channel
     # image = tf.image.rgb_to_grayscale(image)
-    image.set_shape([self.image_size[0], self.image_size[1], 1])
+    # image.set_shape([self.image_size[0], self.image_size[1], 1])
     return image
 
 class SplitedReader(Reader):
